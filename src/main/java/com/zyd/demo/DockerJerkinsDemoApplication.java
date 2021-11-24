b54af99e5801f971ae31900f19358fc8ca5122b5
zedzheng <1019661738@qq.com>
2021-11-24 12:37:58 +0800

    1

--- src/main/java/com/zyd/demo/DockerJerkinsDemoApplication.java
+++ src/main/java/com/zyd/demo/DockerJerkinsDemoApplication.java
@@ -3,10 +3,14 @@ package com.zyd.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @SpringBootApplication
 public class DockerJerkinsDemoApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnknownHostException {
+        System.out.println(InetAddress.getLocalHost());
         SpringApplication.run(DockerJerkinsDemoApplication.class, args);
     }
 
